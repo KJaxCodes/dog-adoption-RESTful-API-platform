babc35e1fb41b91cbaf91b212a73dcaf2c3d437a
Kelly J <[kelly4410@gmail.com]>
2025-08-04 10:28:34 -0600

    create and style home view

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 const PORT = 3001;
 
 const app = express();
 
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
 // database connection
 const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@test-cluster.zd4zpe4.mongodb.net/${process.env.DB_NAME}`;
 mongoose.connect(dbURI)
-  .then((result) => app.listen(PORT))
-  .catch((err) => console.log(err));
+    .then((result) => app.listen(PORT))
+    .catch((err) => console.log(err));
 
 // routes
 //('*') = all the routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
