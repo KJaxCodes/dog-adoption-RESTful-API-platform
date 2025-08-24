b802f8a0df3cadf851d956596a7312f395e74e88
Kelly J <[kelly4410@gmail.com]>
2025-08-24 15:56:12 -0600

    test sign up working, test log in not working

--- test/dog.test.js
+++ test/dog.test.js
@@ -8,20 +8,6 @@ const should = require('chai').should();
 const app = require('../app');
 chai.use(chaiHttp);
 
-// ensure no dogs in DB before testing
-before((done) => {
-    dogs.deleteMany({}, function(err) {});
-    done();
-});
-
-
-// ensure no dogs in DB after testing
-after((done) => {
-    dogs.deleteMany({}, function(err) {});
-    done();
-});
-
-
 
 describe('Authenticated Endpoints', () => {
 
@@ -38,8 +24,9 @@ describe('Authenticated Endpoints', () => {
             .end((err, res) => {
 
                 // Asserts
-                expect(res.status).to.be.equal(200);
+                expect(res.status).to.be.equal(201);
                 expect(res.body).to.be.a('object');
+                console.log("user has signed up");
 
                 //Log in the user
                 chai.request(app)
@@ -49,51 +36,13 @@ describe('Authenticated Endpoints', () => {
                         "password": "Test123"
                     })
                     .end((err, res) => {
-                        expect(res.status).to.be.equal(201);
+                        expect(res.status).to.be.equal(200);
                         let token = res.body.data.token;
 
-                        //Register new dog - name, size, age, description
-                        let newDog = {
-                            name: "Test Doggo",
-                            size: "small",
-                            age: 2,
-                            description: "A very cute dog",
-                        };
-
-                        chai.request(app)
-                            .get('/dogs')
-                            .end((err, res) => {
-                                if (err) return done(err);
-                                res.should.have.status(201)
-                                res.body.should.be.a('object');
-                                done();
-                            });
+                        //Register new dog - name, size, age, description, registeredBy, adoptedBy
+                        //let newDog = {
+                        // }
                     });
             });
     });
 });
-
-
-// let authToken;
-
-// before((done) => {
-//     chai.request(app)
-//         .post('/signup')
-//         .send({ username: 'testuser', password: 'test123' })
-//         .end((err, res) => {
-//             authToken = res.body.token;
-//             console.log(authToken);
-//             done();
-//         });
-// });
-
-
-// it('should verify that the dogs page renders an object successfully', (done) => {
-//     chai.request(app)
-//         .get('/dogs')
-//         .end((err, res) => {
-//             // res.should.have.status(500);
-//             res.body.should.be.a('object');
-//             done();
-//         });
-// 
