e887e7dab13133aa9ba21d82aa341d853669ace7
Kelly J <[kelly4410@gmail.com]>
2025-08-24 14:52:56 -0600

    render home, log in, sign up tests

--- test/dog.test.js
+++ test/dog.test.js
@@ -7,41 +7,18 @@
 // const app = require('../app');
 // chai.use(chaiHttp);
 
-//ensure no dogs in DB before testing
+// // ensure no dogs in DB before testing
 // before((done) => {
 //     Dog.deleteMany({}, function(err) {});
 //     done();
 // });
 
-//ensure no dogs in DB after testing
+// // ensure no dogs in DB after testing
 // after((done) => {
 //     Dog.deleteMany({}, function(err) {});
 //     done();
 // });
 
-// describe('GET / (Home Page)', () => {
-// it('expect the home page to render with welcome message', (done) => {
-//     console.log(process.env.NODE_ENV);
-//     console.log("Test running");
-//     chai.request(app)
-//         .get('/')
-//         .end((err, res) => {
-//             expect(res.text).to.include('Welcome');
-//             done();
-//         });
-// });
-
-
-// it('home page should render status 200', (done) => {
-//     console.log("should status 200 test running");
-//     chai.request(app)
-//         .get('/')
-//         .end((err, res) => {
-//             res.should.have.status(200)
-//             done();
-//         });
-// });
-// });
 
 
 // describe('GET / (Dogs Page)', () => {
