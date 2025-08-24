94a98c6f058b2e19702539d1b57eef5e00e72c62
Kelly J <[kelly4410@gmail.com]>
2025-08-24 15:39:10 -0600

    attempt to test authenticated endpoints

--- test/dog.test.js
+++ test/dog.test.js
@@ -1,4 +1,5 @@
 
+const jwt = require('jsonwebtoken');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { expect } = chai;
@@ -7,42 +8,92 @@ const should = require('chai').should();
 const app = require('../app');
 chai.use(chaiHttp);
 
-// // ensure no dogs in DB before testing
+// ensure no dogs in DB before testing
+before((done) => {
+    dogs.deleteMany({}, function(err) {});
+    done();
+});
+
+
+// ensure no dogs in DB after testing
+after((done) => {
+    dogs.deleteMany({}, function(err) {});
+    done();
+});
+
+
+
+describe('Authenticated Endpoints', () => {
+
+    it('should register + login a user + show any registered dogs', (done) => {
+
+        //Register new user
+        let user = {
+            email: "test@google.com",
+            password: "Test123"
+        }
+        chai.request(app)
+            .post('/signup')
+            .send(user)
+            .end((err, res) => {
+
+                // Asserts
+                expect(res.status).to.be.equal(200);
+                expect(res.body).to.be.a('object');
+
+                //Log in the user
+                chai.request(app)
+                    .post('/login')
+                    .send({
+                        "email": "test@google.com",
+                        "password": "Test123"
+                    })
+                    .end((err, res) => {
+                        expect(res.status).to.be.equal(201);
+                        let token = res.body.data.token;
+
+                        //Register new dog - name, size, age, description
+                        let newDog = {
+                            name: "Test Doggo",
+                            size: "small",
+                            age: 2,
+                            description: "A very cute dog",
+                        };
+
+                        chai.request(app)
+                            .get('/dogs')
+                            .end((err, res) => {
+                                if (err) return done(err);
+                                res.should.have.status(201)
+                                res.body.should.be.a('object');
+                                done();
+                            });
+                    });
+            });
+    });
+});
+
+
+// let authToken;
+
 // before((done) => {
-//     Dog.deleteMany({}, function(err) {});
-//     done();
+//     chai.request(app)
+//         .post('/signup')
+//         .send({ username: 'testuser', password: 'test123' })
+//         .end((err, res) => {
+//             authToken = res.body.token;
+//             console.log(authToken);
+//             done();
+//         });
 // });
 
-// // ensure no dogs in DB after testing
-// after((done) => {
-//     Dog.deleteMany({}, function(err) {});
-//     done();
-// });
 
-
-
-// describe('Authenticated Endpoints', () => {
-//     let authToken;
-
-//     before((done) => {
-//         chai.request(app)
-//             .post('/signup')
-//             .send({ username: 'testuser', password: 'test123' })
-//             .end((err, res) => {
-//                 authToken = res.body.token;
-//                 console.log(authToken);
-//                 done();
-//             });
-//     });
-
-
-    // it('should verify that the dogs page renders an object successfully', (done) => {
-    //     chai.request(app)
-    //         .get('/dogs')
-    //         .end((err, res) => {
-    //             // res.should.have.status(500);
-    //             res.body.should.be.a('object');
-    //             done();
-    //         });
-    // });
-// });
+// it('should verify that the dogs page renders an object successfully', (done) => {
+//     chai.request(app)
+//         .get('/dogs')
+//         .end((err, res) => {
+//             // res.should.have.status(500);
+//             res.body.should.be.a('object');
+//             done();
+//         });
+// 
